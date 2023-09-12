e83547fb050597ce695905d02cdccce9fb000226
antonioIannotta <antonio.iannotta.ai26@gmail.com>
2023-09-12 11:10:50 +0200

    feat: new proxy detection began

---
+++ fairness/proxy/__init__.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def proxy_detection(dataset: pd.DataFrame, protected_attributes: list) -> list:
+    pass
+
+
+def compute_disparate_impact_to_detect_proxy(attribute: pd.Series, protected_attribute: pd.Series) -> float:
+    pass
+
